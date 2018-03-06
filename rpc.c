7793b56b52b6d115d0fa0215303088643ca0c612
zargot <zargot.ripley@gmail.com>
2018-03-06 03:26:16 +0100

    add encode func, validate input, refactor

--- rpc.c
+++ rpc.c
@@ -3,9 +3,10 @@
 // TODO:
 // - test create
 // - test send
-// - validate input
 
 #include <assert.h>
+#include <ctype.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -41,8 +42,51 @@ put(json_object **obj) {
 		json_object_put(*obj);
 }
 
+static bool
+validate_str(string s) {
+	char c;
+	while ((c = *s++)) {
+		if (!isalnum(c) && c != '_')
+			return false;
+	}
+	return true;
+}
+
+static size_t
+encode(char **out, size_t n, ...) {
+	static char buf[1024];
+
+	int len = sprintf(buf, "{");
+	assert(len == 1);
+	va_list ap;
+	va_start(ap, n);
+	while (n--) {
+		let key = va_arg(ap, string);
+		let value = va_arg(ap, string);
+		if (validate_str(key) && validate_str(value)) {
+			assert(len > 0);
+			let diff = snprintf(buf + len, sizeof(buf) - len,
+					"\"%s\":\"%s\",", key, value);
+			if (diff > 0) {
+				len += diff;
+				continue;
+			}
+		}
+		RET_ERR(0, "invalid key/value: %s/%s", key, value);
+	}
+	va_end(ap);
+	len = fmax(len-1, 1);
+	if (len + 2 >= sizeof(buf))
+		RET_ERR(0, "buffer overflow: %d/%zu", len, sizeof(buf));
+	len += sprintf(buf + len, "}");
+	assert(len <= sizeof(buf));
+
+	*out = buf;
+	return len > 0 ? len : 0;
+}
+
 static json_object *
-parse(string str) {
+decode(string str) {
 	enum json_tokener_error err;
 	var json = json_tokener_parse_verbose(str, &err);
 	if (err)
@@ -53,22 +97,21 @@ parse(string str) {
 static size_t
 writeres(void *ptr, size_t size, size_t nmemb, void *userp) {
 	var res = (json_object **)userp;
-	*res = parse((string)ptr);
+	*res = decode((string)ptr);
 	return size * nmemb;
 }
 
 static json_object *
-request(string server, string fmt, va_list args) {
-	static char buf[1024];
-	let len = vsnprintf(buf, sizeof(buf), fmt, args);
-	assert(len < sizeof(buf));
+request(string server, size_t len, string req) {
+	if (!len || !req || !*req)
+		RET_ERR(NULL, "invalid request: %s", req);
 
 	json_object *res = NULL;
 	var ctype = curl_slist_append(NULL, "Content-Type: application/json");
 	curl_easy_setopt(curl, CURLOPT_URL, server);
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, ctype);
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, len);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeres);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res);
 	let err = curl_easy_perform(curl);
@@ -79,11 +122,8 @@ request(string server, string fmt, va_list args) {
 }
 
 static bool
-request_str(string server, string key, size_t len, char *buf, string fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	DEFER_PUT json_object *res = request(server, fmt, args);
-	va_end(args);
+request_str(string server, size_t reqlen, string req, string key, size_t len, char *buf) {
+	DEFER_PUT json_object *res = request(server, reqlen, req);
 	if (!res)
 		RET_ERR(false, "");
 	json_object *value;
@@ -114,37 +154,32 @@ nano_quit() {
 
 bool
 nano_create(char *acc) {
-	return request_str(server,
-			"account", LEN, acc,
-			"{\"action\": \"account_create\", \"wallet\": \"%s\"}",
-			wallet);
+	char *req;
+	let len = encode(&req, 2, "action","account_create", "wallet",wallet);
+	return request_str(server, len, req, "account", LEN, acc);
 }
 
 bool
 nano_balance(string acc, char balance[LEN]) {
-	return request_str(server,
-			"balance", LEN, balance,
-			"{\"action\": \"account_balance\", \"account\": \"%s\"}",
-			acc);
+	char *req;
+	let len = encode(&req, 2, "action","account_balance", "account",acc);
+	return request_str(server, len, req, "balance", LEN, balance);
 }
 
 bool
 nano_send(string acc, string dst, string amount, string guid) {
-	char block[LEN];
-	if (!request_str(server,
-			"block", sizeof(block), block,
-			"{"
-			"\"action\": \"send\","
-			"\"wallet\": \"%s\","
-			"\"source\": \"%s\","
-			"\"destination\": \"%s\","
-			"\"amount\": \"%s\","
-			"\"id\": \"%s\""
-			"}",
-			wallet, acc, dst, amount, guid)) {
+	char res[LEN];
+	char *req;
+	let reqlen = encode(&req, 6,
+			"action", "send",
+			"wallet", wallet,
+			"source", acc,
+			"destination", dst,
+			"amount", amount,
+			"id", guid);
+	if (!request_str(server, reqlen, req, "block", sizeof(res), res))
 		return false;
-	}
-	if (!memcmp(block, INVALID_BLOCK, LEN))
+	if (!memcmp(res, INVALID_BLOCK, LEN))
 		return false;
 	return true;
 }
